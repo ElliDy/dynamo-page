b11b9b66481181ab17cd8e8783a83c07f88243f2
Elisa <hilprecht.elisa@googlemail.com>
2016-08-04 12:14:40 +0200

    Add a viewer for the pictures of the tweets

--- src/app/components/dynamo-tweets/dynamo-tweets.ts
+++ src/app/components/dynamo-tweets/dynamo-tweets.ts
@@ -13,12 +13,44 @@ import {DynamoTweetsService} from './dynamo-tweets.service'
 export class DynamoTweets {
 
 	tweetImages: string[];
+  showImageOverlay: boolean;
+  currentTweetImage: string;
+  currentTweetImageIndex;
 
-  	constructor(private dynamoTweetsService:DynamoTweetsService) {
-  		this.dynamoTweetsService.getTweets().subscribe(
-                     data  => this.tweetImages = data,
-                     error =>  console.log(error),
-                     () => console.log("Finished."));
-  	}
+	constructor(private dynamoTweetsService:DynamoTweetsService) {
+    this.showImageOverlay = false;
+    this.currentTweetImage = "";
+    this.currentTweetImageIndex = 0;
+		this.dynamoTweetsService.getTweets().subscribe(
+                   data  => this.tweetImages = data,
+                   error =>  console.log(error),
+                   () => console.log("Finished."));
+	}
+
+  showTweetImageOverlay(image) {
+    this.currentTweetImage = image;
+    this.currentTweetImageIndex = this.tweetImages.indexOf(this.currentTweetImage);
+    this.showImageOverlay = true;
+  }
+
+  closeOverlayImage(){
+    this.showImageOverlay = false;
+  }
+
+  nextOverlayImage(){
+    this.currentTweetImageIndex++;
+    if(this.currentTweetImageIndex>this.tweetImages.length-1){
+      this.currentTweetImageIndex = 0;
+    }
+    this.currentTweetImage = this.tweetImages[this.currentTweetImageIndex];
+  }
+
+  previousOverlayImage(){
+    this.currentTweetImageIndex--;
+    if(this.currentTweetImageIndex<0){
+      this.currentTweetImageIndex = this.tweetImages.length-1;
+    }
+    this.currentTweetImage = this.tweetImages[this.currentTweetImageIndex];
+  }
 
 }
